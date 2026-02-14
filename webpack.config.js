a022c40d8080cac38f46f8c7dc3c0d5246c0d5ad
Nika Beridze <nikaberidze78@gmail.com>
2026-02-14 20:34:54 +0400

    Сhore: Init React+TS project with webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+    entry: path.resolve(process.cwd(), "src/index.tsx"),
+    output: {
+        path: path.resolve(process.cwd(), "dist"),
+        filename: "bundle.[contenthash].js",
+        clean: true,
+        publicPath: "/"
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ["@babel/preset-env", { targets: "defaults" }],
+                            ["@babel/preset-react", { runtime: "automatic" }],
+                            ["@babel/preset-typescript"]
+                        ]
+                    }
+                }
+            },
+            { test: /\.css$/, use: ["style-loader", "css-loader"] }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+        port: 3000,
+        hot: true,
+        client: { overlay: true }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(process.cwd(), "public/index.html")
+        })
+    ]
+};
