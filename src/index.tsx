e60bf731ea56c565c5710ad911e01bd67ff2959f
Nika Beridze <nikaberidze78@gmail.com>
2026-02-14 20:46:52 +0400

    Chore: Add prettier and basic ignores

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 function App() {
-    return (
-        <div style={{ padding: 24, fontFamily: "system-ui" }}>
-            <h1>Frontend test scaffold</h1>
-            <p>Webpack + React + TS</p>
-        </div>
-    );
+  return (
+    <div style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>Frontend test scaffold</h1>
+      <p>Webpack + React + TS</p>
+    </div>
+  );
 }
 
 createRoot(document.getElementById("root")!).render(<App />);
